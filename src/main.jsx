7733726b05ef10f8e7a20a503e4d9be731d7c1fc
AnuBapatla <anusrichatala@gmail.com>
2026-01-19 22:18:51 -0500

    displayong data in old model

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,8 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
- const App  = () => (
-    <div>
-        <h1> hello world</h1>
-    </div>
- )
+ const App  = () => {
+    return React.createElement('h1',{className:'title'},'welcomee to code revolution');
+ }
 const root= ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App/>);
 export default App;
