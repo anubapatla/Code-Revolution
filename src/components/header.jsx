3135b1a02b9f6b12f3776ab57cd96637a3c41219
AnuBapatla <anusrichatala@gmail.com>
2026-01-21 18:21:34 -0500

    single expression

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,4 +1,10 @@
-const Header =()=> (
-    <div> header</div>
+const getTheYear = () =>{
+  const newDate = new Date();
+  return newDate.getFullYear();
+
+} 
+const Header =() =>(
+    <div> the date is {getTheYear()}</div>
 )
+   
 export default Header;
